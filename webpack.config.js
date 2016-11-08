92ba4fa0a29eab5aa5033d7b48a4357023992d27
Lee Tsai <lee.x.tsai@gmail.com>
2016-11-07 23:58:38 -0800

    [Setup] Configured webpack and setting up index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
 var webpack = require('webpack');
 module.exports = {
+
   // tells webpack where the entry file is
   entry: [
     'webpack/hot/only-dev-server',
     "./js/app.js"
   ],
 
+  // tells webpack where the bundled files should go when our app is built
   output: {
     path: __dirname + '/build',
     filename: "bundle.js"
   },
 
+  // add and remove loaders based on what we need webpack to bundle for us
   module: {
     loaders: [
+      // react-hot and babel allow us to use ES6 while hot loading
       { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
       { test: /\.css$/, loader: "style!css" }
     ]
   },
 
+  // different types of webpack plugins
   plugins: [
+    // hot loader would not automatically reload if there are errors in the code
     new webpack.NoErrorsPlugin()
   ]
 };
