af54a93a295993e80a191676fd38495cfad37520
Lee Tsai <lee.x.tsai@gmail.com>
2016-11-22 12:17:39 -0800

    [Routing] Managed routes to reuse layout in MainLayout

--- src/router.js
+++ src/router.js
@@ -4,19 +4,22 @@ import { Router, Route, browserHistory, IndexRedirect } from 'react-router';
 
 // Components
 import App from './components/App';
-import Home from './components/Home';
+import MainLayout from './components/MainLayout';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
 import Resume from './components/Resume';
+import Home from './components/Home';
 
 // Routes
 const routes = (
-  <Router history={browserHistory}>
-    <Route component={App}>
-      <Route path="/" component={Home} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/about" component={About} />
-      <Route path="/resume" component={Resume} />
+  <Router history={ browserHistory }>
+    <Route component={ App }>
+      <Route component={ MainLayout }>
+        <Route path="/" component={ Home } />
+        <Route path="/resume" component={ Resume } />
+        <Route path="/about" component={ About } />
+        <Route path="/portfolio" component={ Portfolio } />
+      </Route>
     </Route>
   </Router>
 );
