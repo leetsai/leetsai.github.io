8c7f5ef5a2a6444f148a2492675de4646ebd7e07
Lee Tsai <lee.x.tsai@gmail.com>
2016-11-30 15:50:57 -0800

    [Content] Added Lee's biography and social media links

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,16 @@ import { Link } from 'react-router';
 class Home extends Component {
   render() {
     return (
-      <div className="main">
-        <div className="container">
-          <div className="biography">
-            <img id="profile_picture" src="src/css/leetsai.png" alt="profile_picture" />
-            <p id="first_line">Hey, I'm Lee,</p>
-            <p id="second_line">an American full-stack developer working</p>
-            <p id="third_line">in San Francisco</p>
+      <div className="biography">
+        <img id="profile_picture" src="src/css/leetsai.png" alt="profile_picture" />
+        <p id="first_line">Hi, I'm Lee!</p>
+        <p id="second_line">an American full-stack developer working</p>
+        <p id="third_line">in San Francisco</p>
 
-            <div className="social_media">
-              <a href="https://linkedin.com/in/leetsai"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
-              <i className="fa fa-github fa-2x" aria-hidden="true"></i>
-              <i className="fa fa-angellist fa-2x" aria-hidden="true"></i>
-            </div>
-          </div>
+        <div className="social_media">
+          <a href="https://linkedin.com/in/leetsai" target="_blank"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true" id="linkedin"></i></a>
+          <a href="https://github.com/leetsai" target="_blank"><i className="fa fa-github fa-2x" aria-hidden="true" id="github"></i></a>
+          <a href="https://angel.co/leetsai" target="_blank"><i className="fa fa-angellist fa-2x" aria-hidden="true" id="angellist"></i></a>
         </div>
       </div>
     );
