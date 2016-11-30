223d4871264093abe0239234290a7285bedcbc93
Lee Tsai <lee.x.tsai@gmail.com>
2016-11-30 15:48:33 -0800

    [Clean-up] Extraneous divs deleted

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -4,28 +4,61 @@ import { Link } from 'react-router';
 class Home extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <ul className="navigation">
-            <li className="pages"><Link to="/blog">Blog</Link></li>
-            <li className="pages"><Link to="/portfolio">Portfolio</Link></li>
-            <li className="pages"><Link to="/about">About</Link></li>
-            <li className="pages"><Link to="/">Home</Link></li>
-          </ul>
+      <div className="container">
+        <div className="col">
+          {this.props.children}
+
+          <footer>
+            <div id="copyright">
+              © COPYRIGHT 2016-17
+            </div>
+          </footer>
         </div>
 
-        <main>
-          {this.props.children}
-        </main>
-
-        <footer>
-          <div id="copyright">
-            © COPYRIGHT 2016-17
+        <div className="col">
+          <div className="tiles">
+            <div className="tile">
+              <Link to="/">
+                <div className="tile_content">Home</div>
+              </Link>
+            </div>
+            <div className="tile">
+              <Link to="/about">
+                <div className="tile_content">About</div>
+              </Link>
+            </div>
+            <div className="tile">
+              <Link to="/portfolio">
+                <div className="tile_content">Portfolio</div>
+              </Link>
+            </div>
+            <div className="tile">
+              <Link to="/blog">
+                <div className="tile_content">Blog</div>
+              </Link>
+            </div>
           </div>
-        </footer>
+        </div>
       </div>
     );
   }
 }
 
 export default Home;
+
+
+
+
+// <div className="navbar">
+//   <ul className="navigation">
+//     <li className="pages"><Link to="/blog">Blog</Link></li>
+//     <li className="pages"><Link to="/portfolio">Portfolio</Link></li>
+//     <li className="pages"><Link to="/about">About</Link></li>
+//     <li className="pages"><Link to="/">Home</Link></li>
+//   </ul>
+// </div>
+
+
+// <div>
+//   <div className="tiles"><Link to="/about">WHERE ARE YOU</Link></div>
+// </div>
