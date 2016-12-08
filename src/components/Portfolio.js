dfe3c5aa80afa60fc77e12bf050f5c40d70a850e
Lee Tsai <lee.x.tsai@gmail.com>
2016-12-08 10:18:51 -0800

    [Production] Pushed to github

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,9 +14,9 @@ class Portfolio extends Component {
           </div>
 
           <div className="project_right">
-            <div className="write-up">
-               <p>Wanderlist is a platform for authors and readers to create, comment, and vote on interesting lists</p>
-            </div>
+             <p>Wanderlist is a platform for authors and readers to create, comment, and vote on interesting lists</p><br></br>
+             <p>Built in JavaScript and with React and Redux</p><br></br>
+             <p>Check out the code-base here</p>
           </div>
         </div>
 
@@ -28,9 +28,9 @@ class Portfolio extends Component {
           </div>
 
           <div className="project_right">
-            <div className="write-up">
-              <p>Spontaneo is an idea generating tool designed to help users discover fun activities and top-rated restaurants</p>
-            </div>
+            <p>Spontaneo is an idea generating tool designed to help users discover fun activities and top-rated restaurants</p><br></br>
+            <p>Built in JavaScript and with Angular</p><br></br>
+            <p>Check out the code-base here</p>
           </div>
         </div>
       </div>
@@ -39,36 +39,3 @@ class Portfolio extends Component {
 }
 
 export default Portfolio;
-
-
-// <img className="thumbnail" src="src/wanderlist2.png" alt="Image" />
-
-
-// import React, { Component } from 'react';
-//
-// class Portfolio extends Component {
-//   render() {
-//     return (
-//       <div className="projects">
-//         <h1> Projects by Lee </h1>
-//         <div className="project">
-//           <img className="thumbnail" src="src/wanderlist2.png" alt="Image" />
-//           <div className="write-up">
-//              <p>Wanderlist is a platform for authors and readers to create, comment, and vote on interesting lists</p>
-//           </div>
-//         </div>
-//
-//         <div className="project">
-          // <h2 className="project_title"> Spontaneo </h2>
-          // <h3 className="project_caption"> "Need an adventure?" </h3>
-//           <img className="thumbnail" src="src/spontaneo.png" alt="Image" />
-//           <div className="write-up">
-//             <p>Spontaneo is an idea generating tool designed to help users discover fun activities and top-rated restaurants</p>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default Portfolio;
