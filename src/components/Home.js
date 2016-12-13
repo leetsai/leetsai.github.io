688300331d5cd578015451e4b39b507f6f4ac8d2
Lee Tsai <lee.x.tsai@gmail.com>
2016-12-12 17:32:19 -0800

    [Styling] Redesigned home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,24 +5,24 @@ class Home extends Component {
   render() {
     return (
       <div className="biography">
-        <img id="profile_picture" src="src/css/leetsai.png" alt="profile_picture" />
-
         <p className="intro">
-          <span id="first_line">Hi, I'm Lee!</span> <br></br>
-          <span id="the_rest">
-            a full-stack web developer <br></br>
-            working in San Francisco
+          <span id="first_line">Hi.</span><br></br>
+          <span className="the_rest">
+            I'm Lee, a hacker<br></br>
+            living in San Francisco
           </span>
         </p>
-
-        <div className="social_media">
-          <a href="https://linkedin.com/in/leetsai" target="_blank"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true" id="linkedin"></i></a>
-          <a href="https://github.com/leetsai" target="_blank"><i className="fa fa-github fa-2x" aria-hidden="true" id="github"></i></a>
-          <a href="https://angel.co/leetsai" target="_blank"><i className="fa fa-angellist fa-2x" aria-hidden="true" id="angellist"></i></a>
-        </div>
       </div>
     );
   }
 }
 
 export default Home;
+
+
+// <img id="profile_picture" src="src/css/leetsai.png" alt="profile_picture" />
+// <div className="social_media">
+//   <a href="https://linkedin.com/in/leetsai" target="_blank"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true" id="linkedin"></i></a>
+//   <a href="https://github.com/leetsai" target="_blank"><i className="fa fa-github fa-2x" aria-hidden="true" id="github"></i></a>
+//   <a href="https://angel.co/leetsai" target="_blank"><i className="fa fa-angellist fa-2x" aria-hidden="true" id="angellist"></i></a>
+// </div>
